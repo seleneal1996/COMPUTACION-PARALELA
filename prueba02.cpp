6cfe9de7cb91713ca651a43eec18cc8143675a89
Selene <sbarrios@unsa.edu.pe>
2022-09-21 08:20:20 -0500

    Ejercicio 1:Implementar y comparar los 2-bucles anidados FOR presentados en el cap. 2 del libro, pag 22

---
+++ prueba02.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int MAX = 1000;
+    double A[MAX][MAX], x[MAX], y[MAX];
+    for (int i = 0; i < MAX; i++)
+        for (int j = 0; j < MAX; j++)
+            y[i] += A[i][j]*x[j];
+}
